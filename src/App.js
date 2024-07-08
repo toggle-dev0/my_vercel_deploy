525b5b2dd60ce75927027033d3bdfbb9280d5ff0
toggle-dev0 <munachienabulele2505@gmail.com>
2024-07-08 15:48:06 +0100

    Made some changes to the folder structure
    Created new files for each page, just the homepage for now

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import "./components/Header/Header.css";
-import Main from "./components/Main/Main";
-import "./components/Main/Main.css";
-import Section from "./components/Main/Section/Section";
-import "./components/Main/Section/Section.css";
 import Footer from "./components/Footer/Footer";
 import "./components/Footer/Footer.css";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main>
-        <Section>
-          <div className="Section-image"></div>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium ipsa quisquam optio omnis modi nemo, excepturi necessitatibus molestiae exercitationem est eaque maxime minus nihil maiores tempore ea eligendi quibusdam eum facilis! Dignissimos impedit placeat fugiat.</p>
-        </Section>
-        <Section>
-          <div className="Section-image"></div>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium ipsa quisquam optio omnis modi nemo, excepturi necessitatibus molestiae exercitationem est eaque maxime minus nihil maiores tempore ea eligendi quibusdam eum facilis! Dignissimos impedit placeat fugiat.</p>
-        </Section>
-        <Section>
-          <div className="Section-image"></div>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium ipsa quisquam optio omnis modi nemo, excepturi necessitatibus molestiae exercitationem est eaque maxime minus nihil maiores tempore ea eligendi quibusdam eum facilis! Dignissimos impedit placeat fugiat.</p>
-        </Section>
-        <Section>
-          <div className="Section-image"></div>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium ipsa quisquam optio omnis modi nemo, excepturi necessitatibus molestiae exercitationem est eaque maxime minus nihil maiores tempore ea eligendi quibusdam eum facilis! Dignissimos impedit placeat fugiat.</p>
-        </Section>
-      </Main>
+      <Homepage />
       <Footer />
     </div>
   );
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import Main from '../components/Main/Main'
+import "../components/Main/Main.css"
+import Section from "../components/Section/Section.jsx"
+import "../components/Section/Section.css"
+import "../styles/Homepage.css"
+
+const Homepage = () => {
+  return (
+    <Main>
+        <Section>
+            <div className="Section-image"></div>
+            <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium ipsa
+            quisquam optio omnis modi nemo, excepturi necessitatibus molestiae
+            exercitationem est eaque maxime minus nihil maiores tempore ea eligendi
+            quibusdam eum facilis! Dignissimos impedit placeat fugiat.
+            </p>
+        </Section>
+        <Section>
+            <div className="Section-image"></div>
+            <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium ipsa
+            quisquam optio omnis modi nemo, excepturi necessitatibus molestiae
+            exercitationem est eaque maxime minus nihil maiores tempore ea eligendi
+            quibusdam eum facilis! Dignissimos impedit placeat fugiat.
+            </p>
+        </Section>
+        <Section>
+            <div className="Section-image"></div>
+            <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium ipsa
+            quisquam optio omnis modi nemo, excepturi necessitatibus molestiae
+            exercitationem est eaque maxime minus nihil maiores tempore ea eligendi
+            quibusdam eum facilis! Dignissimos impedit placeat fugiat.
+            </p>
+        </Section>
+        <Section>
+            <div className="Section-image"></div>
+            <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium ipsa
+            quisquam optio omnis modi nemo, excepturi necessitatibus molestiae
+            exercitationem est eaque maxime minus nihil maiores tempore ea eligendi
+            quibusdam eum facilis! Dignissimos impedit placeat fugiat.
+            </p>
+        </Section>
+    </Main>
+  )
+}
+
+export default Homepage
